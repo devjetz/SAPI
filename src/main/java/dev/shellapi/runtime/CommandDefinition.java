b7ad04f26c31fff04f53b40d53eef6f9b68ca2b1
dev <109991303+devjetz@users.noreply.github.com>
2026-06-12 20:16:24 +0000

    Update CommandDefinition.java

--- src/main/java/dev/shellapi/runtime/CommandDefinition.java
+++ src/main/java/dev/shellapi/runtime/CommandDefinition.java
@@ -1,1 +1,32 @@
+package dev.shellapi.runtime;
 
+import java.util.List;
+
+public class CommandDefinition {
+
+    public String shell;
+    public List<Arg> arguments;
+
+    public static class Arg {
+        public String name;
+        public boolean required;
+        public boolean greedy;
+    }
+
+    public boolean validateArgs(String[] args) {
+
+        if (arguments == null) return true;
+
+        int requiredCount = 0;
+
+        for (Arg a : arguments) {
+            if (a.required) requiredCount++;
+        }
+
+        if (args.length < requiredCount) {
+            return false;
+        }
+
+        return true;
+    }
+}
