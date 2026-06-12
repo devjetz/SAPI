e0d6473b20cfa94f8d14b9b1d005d1d16cfb5bed
dev <109991303+devjetz@users.noreply.github.com>
2026-06-12 20:16:49 +0000

    Update FileUtil.java

--- src/main/java/dev/shellapi/util/FileUtil.java
+++ src/main/java/dev/shellapi/util/FileUtil.java
@@ -1,1 +1,13 @@
+package dev.shellapi.util;
 
+import java.io.File;
+
+public class FileUtil {
+
+    public static void ensureDir(String path) {
+        File f = new File(path);
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+    }
+}
