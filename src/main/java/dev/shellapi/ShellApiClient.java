788bbf5b8a12c10f0111a672c65d5b8d684accce
dev <109991303+devjetz@users.noreply.github.com>
2026-06-12 20:15:26 +0000

    Update ShellApiClient.java

--- src/main/java/dev/shellapi/ShellApiClient.java
+++ src/main/java/dev/shellapi/ShellApiClient.java
@@ -1,1 +1,14 @@
+package dev.shellapi;
 
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShellApiClient implements ClientModInitializer {
+
+    public static final String ROOT = "api_apps";
+
+    @Override
+    public void onInitializeClient() {
+        util.FileUtil.ensureDir(ROOT);
+        intercept.ChatInterceptor.register();
+    }
+}
